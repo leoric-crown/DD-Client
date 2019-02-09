61fdd51bdc43acb1fb32f2c04befe771834a2999
leoric-crown <leon.ricardo314@gmail.com>
2019-02-09 04:33:05 -0600

    Removed some comments

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -3,16 +3,6 @@ export const LOGOUT_USER = 'LOGOUT_USER'
 
 const defaultUserPic = "http://s3.amazonaws.com/37assets/svn/765-default-avatar.png"
 
-// export function setAuthedUser(authedUser, photoURL, isDM, userId) {
-//   return {
-//     type: SET_AUTHED_USER,
-//     authedUser,
-//     photoURL,
-//     isDM,
-//     userId
-//   }
-// }
-
 export function setAuthedUser(user) {
   var { email, photoURL, isDM, userId } = user
   if(!photoURL) photoURL = defaultUserPic
