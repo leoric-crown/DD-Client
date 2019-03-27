85653c280e011e4e17d183db861069c48feaa4bd
leoric-crown <leon.ricardo314@gmail.com>
2019-03-26 19:04:49 -0600

    Added config.js

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    FACEBOOK_APP_ID: process.env.FACEBOOK_APP_ID || "233345377593125",
+    API: process.env.API || "http://localhost:5000",
+    WS: process.env.WS || "ws://localhost:5000"
+}
+
+export default config
