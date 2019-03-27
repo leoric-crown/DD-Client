17c43c6ad7b6ef7f0997c633ad6fdee7059b698a
leoric-crown <leon.ricardo314@gmail.com>
2019-03-26 23:26:41 -0600

    Updated config file

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
-const defaultConfig = require('./config.json')
 const config = {
-    FACEBOOK_APP_ID: process.env.REACT_APP_FACEBOOK_APP_ID || defaultConfig.FACEBOOK_APP_ID,
-    API: process.env.REACT_APP_API || defaultConfig.API,
-    WS: process.env.REACT_APP_WS || defaultConfig.WS
+    FACEBOOK_APP_ID: process.env.REACT_APP_FACEBOOK_APP_ID,
+    API: process.env.REACT_APP_API || 'http://localhost:5000',
+    WS: process.env.REACT_APP_WS || 'ws://localhost:5000'
 }
 
 export default config
